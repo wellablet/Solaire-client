f6ab50b037f3db8f6401a7173bea307c29806bd8
pahirdinhamut <69786935+pahirdinhamut@users.noreply.github.com>
2023-06-18 16:46:27 +0200

    finsh sider bar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Avatar from '../assests/img1.png'
-
+import { BsChatSquareFill } from "react-icons/bs";
+import { FaUser, FaBoxOpen } from "react-icons/fa";
+import { AiTwotoneSetting } from "react-icons/ai";
+import { RiLogoutBoxRFill } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 export default function SideBar() {
   return (
     <div className='bg-primary w-[10%] h-screen min-w-fit'>
@@ -8,6 +12,20 @@ export default function SideBar() {
         <img src={Avatar} className='w-32 h-32 object-cover' alt='' />
         <h3 className='text-white text-md mt-5 '>User Name</h3>
       </header>
+      <div className='flex flex-col h-[65%] justify-between items-center mt-14'>
+        <Link className='h-12 w-12 rounded-full flex items-center justify-center sidebar-icon '>
+          <BsChatSquareFill className='text-white text-2xl mx-auto ' />
+        </Link>
+        <Link className='h-12 w-12 rounded-full flex items-center justify-center sidebar-icon '>
+          <FaUser className='text-white text-2xl mx-auto' />
+        </Link>
+        <Link className='h-12 w-12 rounded-full flex items-center justify-center sidebar-icon '>
+          <AiTwotoneSetting className='text-white text-2xl mx-auto' />
+        </Link>
+        <Link className='h-12 w-12 rounded-full flex items-center justify-center sidebar-icon '>
+          <RiLogoutBoxRFill className='text-white text-2xl mx-auto' />
+        </Link>
+      </div>
     </div>
   )
 }
